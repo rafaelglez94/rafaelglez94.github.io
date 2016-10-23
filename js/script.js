701b24f8c5cf0dca81a4b83e5de1b3576a5a408b
Rafael Gonzalez <rafa.gc2807@gmail.com>
2016-10-23 03:39:31 -0600

    Haciendo lo mejor

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('.lines').html(function(index,value) {
+		for (var i = 0; i < 28; i++) {
+			$('<div class="mdl-grid"><div class="mdl-cell mdl-cell--12-col">'+(i+1)+'</div></div>').appendTo('.lines')
+		}
+	});
+	$(".etq").html(function( index, value ) {
+		return '<span class="etq-i">'+$(this).data('id')+'</span>'+value+'<span class="etq-r">'+$(this).data('id')+'</span>';
+	});
+	$('.etq-i').html(function( index, value ) {
+		return '&lt;<span class="etq-c">'+value+'</span>&gt;' ;
+	});
+	$('.etq-r').html(function( index, value ) {
+		return '&lt;/<span class="etq-c">'+value+'</span>&gt;' ;
+	});
+});
