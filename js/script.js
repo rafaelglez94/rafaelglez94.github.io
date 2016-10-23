16442cfb1ac38063453236541da40ce2283f3261
Rafael Gonzalez <rafa.gc2807@gmail.com>
2016-10-23 07:51:42 -0600

    Ultimo pa dormirme

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$('.lines').html(function(index,value) {
-		for (var i = 0; i < 40; i++) {
+		for (var i = 0; i < 42; i++) {
 			$('<div class="mdl-grid"><div class="mdl-cell mdl-cell--12-col">'+(i+1)+'</div></div>').appendTo('.lines')
 		}
 	});
 	$(".etq").html(function( index, value ) {
-		return '<span class="etq-i">'+$(this).data('id')+'</span>'+value+'<span class="etq-r">'+$(this).data('id')+'</span>';
+		return '<span class="etq-i">'+$(this).data('id')+'</span><span class="etq-cnt">'+value+'</span><span class="etq-r">'+$(this).data('id')+'</span>';
 	});
 	$('.etq-i').html(function( index, value ) {
 		return '&lt;<span class="etq-c">'+value+'</span>&gt;' ;
@@ -14,3 +14,4 @@ $(document).ready(function() {
 		return '&lt;/<span class="etq-c">'+value+'</span>&gt;' ;
 	});
 });
+
