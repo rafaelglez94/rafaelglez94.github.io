62535a3bcd179f0a5c50252142cf499e7c3b803c
Rafael Gonzalez <rafa.gc2807@gmail.com>
2018-12-20 23:04:57 -0600

    nueva pagina

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
-	var porcentaje = 100;
-	var index = 1;
-	var total = 12;
-	var maxSize = 100;
-	var minSize = 30;
-	var maxDelay = 7;
-	var minDelay = 0;
-	var maxDuration = 10;
-	var minDuration = 5;
-	$( ".bg-bubbles li" ).each(function( index ) {
-		var size  = Math.floor((Math.random() * maxSize) + minSize);
-		var delay = Math.floor((Math.random() * maxDelay) + minDelay);
-		var duration = Math.floor((Math.random() * maxDuration) + minDuration);
-  		$( this ).css("left", ((porcentaje/total) * index) +"%");
-  		$( this ).css("width",  size+"px");
-  		$( this ).css("height",  size+"px");
-  		$( this ).css("animation-delay",  delay+"s");
-  		$( this ).css("animation-duration",  duration+"s");
 
-  		index++;
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : (new Date()).getTime() + 08*10*60*60*1000
 	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
 
 
 });
